085165f4d8228fa5d090b813b47513a132f17be2
Zach Neibaur <znn117@hotmail.com>
2020-10-03 23:40:56 -0600

    Additional settings

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const rateLimit = require('express-rate-limit');
 
 // Load env vars
 dotenv.config({ path: './config/config.env'});
@@ -12,6 +16,22 @@ connectDB();
 const replays = require('./routes/replays');
 
 const app = express();
+
+// Sanitize data
+app.use(mongoSanitize());
+
+// Set security headers
+app.use(helmet());
+
+// Prevent XSS attacks
+app.use(xss());
+
+// Rate limiting
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 10 mins
+  max: 100
+});
+app.use(limiter);
 
 // Route files
 app.use('/api/replays', replays);
